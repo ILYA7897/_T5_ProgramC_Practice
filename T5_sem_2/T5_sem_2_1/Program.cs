acb3acdc19f6e3fe9ef5a7be7d4451daf4136275
«ILYA7897» <belobokov_ilya@mail.ru>
2022-03-27 08:10:16 +0300

    ДЗ семинару 2 задача 1

--- T5_sem_2/T5_sem_2_1/Program.cs
+++ T5_sem_2/T5_sem_2_1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Семинар 1 задача 1
+// Задача 1: Напишите программу,
+// которая принимает на вход трёхзначное число и на выходе показывает вторую 
+// цифру этого числа.
+Console.WriteLine("Введите целое трехзначное число = ");
+string strV1 = Console.ReadLine();
+int value1 = int.Parse(strV1) ;
+int result = (value1%100)/10 ; 
+Console.WriteLine($"Вторая цифра введённого числа {result}");    
+Console.WriteLine($"End ");
