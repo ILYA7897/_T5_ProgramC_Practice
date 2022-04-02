ca7ab138a18946052e825626c6808288c3bb94ec
«ILYA7897» <belobokov_ilya@mail.ru>
2022-04-02 13:50:39 +0300

    выполнение дз  по семинару 4_2

---
+++ T5_sem_4/T5_sem_4_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2: Напишите программу, 
+// которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int Sum = 0 ;
+for (int index= 0; index < (N+1); index++)
+    {
+        Sum = Sum + index ;
+    }
+
+Console.WriteLine($"Сумма цифр в числе = {Sum}");
+
+Console.WriteLine($"End");
