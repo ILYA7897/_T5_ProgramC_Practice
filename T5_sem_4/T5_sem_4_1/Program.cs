d30b45a3451e3af29294f8d3ad58f0989d5658c7
«ILYA7897» <belobokov_ilya@mail.ru>
2022-04-02 12:49:05 +0300

    выполнение дз  по семинару 4_1

---
+++ T5_sem_4/T5_sem_4_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 1: Напишите цикл, 
+//который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Write("Введите А: ");
+double A = Convert.ToDouble(Console.ReadLine());
+double S = 1;
+Console.Write("Введите B (степень числа А) = ");
+int B = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < B; i++)
+{
+S *= A;
+}
+Console.WriteLine($"А в степени В = {S}");
+
+
+Console.WriteLine($"End");
