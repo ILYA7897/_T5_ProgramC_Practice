8ce75a4523acc457d90a17a432ac2e26a81c23c3
«ILYA7897» <belobokov_ilya@mail.ru>
2022-04-02 11:43:25 +0300

    выполнение дз  по семинару 3_3

---
+++ T5_sem_3/T5_sem_3_3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 3: Напишите программу, 
+//которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите ЧИСЛО N =");
+string strN = Console.ReadLine();
+int N = int.Parse(strN) ;
+
+for (int i=1; i<(N+1); i++)
+{
+int result = i*i*i ;
+Console.WriteLine(result);
+}
+Console.WriteLine($"End ");
+
