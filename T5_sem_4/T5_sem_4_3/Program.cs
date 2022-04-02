d3120cc811c0e23564d80bfacef4031a9f02adcf
«ILYA7897» <belobokov_ilya@mail.ru>
2022-04-02 14:21:25 +0300

    выполнение дз  по семинару 4_3

---
+++ T5_sem_4/T5_sem_4_3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 3: Напишите программу,
+// которая задаёт массив из 8 элементов случайными числами и выводит их на экран.
+// Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах)
+
+void recARAY  (int[] array)
+{
+    int Length = array.Length ;
+    for (int index= 0; index < Length; index++)
+    {
+    array[index] = new Random().Next(1, 10) ;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position =0;
+    while (position < count)
+    {
+        Console.Write($"  {col[position]}") ;
+        position++ ;
+    }
+}
+
+int[] arrayN = new int [8] ;
+recARAY (arrayN) ;
+PrintArray(arrayN);
+Console.WriteLine() ;
+
+Console.WriteLine($"End");
