7181038d64ed943b9cc4ac8c5763a19ac9f6f58b
«ILYA7897» <belobokov_ilya@mail.ru>
2022-04-03 12:03:02 +0300

    выполнение дз  по семинару 6 задача 6_1

---
+++ T5_sem_6/T5_sem_6_1/Program.cs
@@ -0,0 +1,45 @@
+﻿//СЕМИНАР 6
+// Задача 1: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+void recARAY1 (int[] array1)
+{
+int Length = array1.Length ;
+    for (int z = 0; z < Length; z++) 
+    {
+    Console.Write("Введите любое число = ");
+    array1[z] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+void printARAY1 (int[] array1)
+{
+int Length = array1.Length ;
+    for (int z = 0; z < Length; z++) 
+    {
+    Console.Write($"    {array1[z]}");
+    }
+}
+
+Console.Write("Введите любые числа M раз. Число М = ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array1 = new int [M];
+
+recARAY1 (array1) ;
+printARAY1 (array1) ;
+Console.WriteLine("  ");
+
+int count = 0;
+int z = 0;
+for (z = 0; z<array1.Length ; z++)
+{
+    if (array1[z]>0)
+    {
+        count++ ;
+    }
+}
+Console.WriteLine($"Количество чисел больше 0 =  {count}");
+
+Console.WriteLine("  ");
+Console.Write($"End");
+
