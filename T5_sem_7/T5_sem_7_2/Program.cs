ef09980370f342a978f5086f21098bb4366ba61a
«ILYA7897» <belobokov_ilya@mail.ru>
2022-04-07 02:46:02 +0300

    выполнение дз  по семинару 7 задача 7_2 (с использованием функции)

--- T5_sem_7/T5_sem_7_2/Program.cs
+++ T5_sem_7/T5_sem_7_2/Program.cs
@@ -1,44 +1,52 @@
 ﻿// Семинар 7 
-//Задача 7_2: 
-//Задача 2: Напишите программу, 
+//Задача 7_2: Напишите программу, 
 // которая на вход принимает позиции элемента в двумерном массиве,
 // и возвращает значение этого элемента или же указание, что такого элемента нет.
 
+// Функция recARAY1 заполняет массив случайными числами и печатает его для проверки.
+void recARAY1 (double[,] array)
+{
+    if (array is null)
+    {
+        throw new ArgumentNullException(nameof(array));
+    }
+
+    int m = array.GetLength(0); 
+Console.WriteLine($"Определение размерности массива M[mxn]. m = {m}");
+int n = array.GetLength(1); 
+Console.WriteLine($"Определение размерности массива M[mxn]. n = {n}");
+for (int a = 0; a < m; a++)
+{
+    for (int b = 0; b < n; b++)
+    {
+    array[a,b] = new Random().NextDouble() * 100; // NextDouble() дает случайное вещественное число в диапазоне от 0 до 1
+    Console.Write("{0,6:F2}", array[a,b]);
+    }
+Console.WriteLine() ;
+}
+}
+// Создаем двумерный массив
 Console.WriteLine("Зададим двумерный массив m x n . Введите число m = ") ;
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Зададим двумерный массив m x n . Введите число n = ") ;
 int n = Convert.ToInt32(Console.ReadLine());
 double[,] array = new double[m,n];
 
-// Заполняем массив с заданной размерностью случайными числами. Печатаем этот массив (для проверки)
-//recARAY1(array);
-// для m
-int mm = array.GetLength(0); 
-Console.WriteLine($"Определение размерности массива M[mxn]. m = {mm}");
-// для n
-int nn = array.GetLength(1); 
-Console.WriteLine($"Определение размерности массива M[mxn]. n = {nn}");
-for (int ii = 0; ii < mm; ii++)
-{
-    for (int jj = 0; jj < nn; jj++)
-    {
-    array[ii,jj] = new Random().NextDouble() * 100; // NextDouble() дает случайное вещественное число в диапазоне от 0 до 1
-    Console.Write("{0,6:F2}", array[ii,jj]);
-    }
-Console.WriteLine() ;
-}
+// Вызываем функцию для заполнения массива заданной размерности случайными числами. 
+// В функции прописана печать массива для проверки.
+recARAY1(array);
 
 // Получение позиции элемента в двумерном массиве 
-Console.WriteLine("Введите запрашиваемую позицию элемента в двумерном массиве M[i; j]. Введите число i = ") ;
+Console.WriteLine("Введите запрашиваемую позицию элемента в двумерном массиве M[i; j] (ПОЗИЦИЯ НАЧИНАЕТСЯ С 0 ЗНАЧЕНИЯ). Введите число i = ") ;
 int i = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите запрашиваемую позицию элемента в двумерном массиве M[i; j]. Введите число j = ") ;
+Console.WriteLine("Введите запрашиваемую позицию элемента в двумерном массиве M[i; j].(ПОЗИЦИЯ НАЧИНАЕТСЯ С 0 ЗНАЧЕНИЯ). Введите число j = ") ;
 int j = Convert.ToInt32(Console.ReadLine());
 // Возврат значения запрашиваемого элемента или же указание, что такого элемента нет
-//int m = array1.GetLength(0); 
-//int n = array1.GetLength(1); 
-if (i < mm)
+int s = array.GetLength(0); 
+int k = array.GetLength(1); 
+if (i < s)
 {
-    if (j < nn)
+    if (j < k)
     {
     Console.Write("{0,6:F2}", array[i,j]);
     Console.WriteLine("  ");        
