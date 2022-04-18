345af73d3216c45b5727749033fbdd1c23d1ecda
«ILYA7897» <belobokov_ilya@mail.ru>
2022-04-18 04:47:42 +0300

    выполнение дз  по семинару 8 задача 8_1

--- T5_sem_8/T5_sem_8_1/Program.cs
+++ T5_sem_8/T5_sem_8_1/Program.cs
@@ -1,89 +1,72 @@
 ﻿// Семинар  8
-// Задача 2: Задайте прямоугольный двумерный массив. Напишите программу,
-// которая будет находить строку с наименьшей суммой элементов.
+// Задача 8_1: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
 
-void PrintARAY (double [] array)
+void PrintARAY (double [ , ] array)
 {
      int m = array.GetLength(0); 
 //    Console.WriteLine($"Определение размерности одномерного массива с суммой строк = {m}");
-    for (int a = 0; a < m; a++)
+    for (int i = 0; i < m; i++)
     {
-        Console.Write(array[a]);
+        for (int j = 0; j < m; j++)
+        {
+        Console.Write(array[i,j]);
         Console.Write("      ") ;
+        }
+        Console.WriteLine("      ") ;
     }
 }
 
-
+static double[,] SortUbiv(double[,] x)
+{
+    double minVal = x[0, 0];
+    for (int i = 0; i < x.GetLength(0); i++)// перечисление строк
+    {
+        for (int j = 0; j < x.GetLength(1); j++)// перечисление символов(столбцов)
+        {
+            for (int q = i; q < x.GetLength(0); q++)//перечесления строк для проверки
+            {
+                for (int w = (q == i) ? j : 0; w < x.GetLength(1); w++)   // перечесление смиволов для проверки (исключаем проверенные и заменннеые символы)
+                {
+ 
+                    if (x[i, j] < x[q, w])
+                    {
+                        minVal = x[q, w];
+                        x[q, w] = x[i, j];
+                        x[i, j] = minVal;
+                    }
+ 
+                }
+            }
+ 
+        }
+    }
+            
+    return x;
+}
+        
 Console.WriteLine("Зададим двумерный массив m x n . Введите число m = ") ;
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Зададим двумерный массив m x n . Введите число n = ") ;
 int n = Convert.ToInt32(Console.ReadLine());
-double[,] array = new double[m,n];
+double[,] array1 = new double[m,n];
 for (int i = 0; i < m; i++)
 {
     for (int j = 0; j < n; j++)
     {
-    array[i,j] = new Random().Next(0,10);
-    Console.Write("{0,6:F2}", array[i,j]);
+    array1[i,j] = new Random().Next(0,10);
+ //   Console.Write("{0,6:F2}", array1[i,j]);
     }
-Console.WriteLine() ;
+//Console.WriteLine() ;
 }
 Console.WriteLine() ;
 
-// Определяем кол-во строк М
-int M = array.GetLength(0); 
-// проверка присваемого числа 
-//Console.WriteLine($"Определение размерности массива M[mxn]. m = {M}");
+PrintARAY(array1) ;
+Console.WriteLine() ;
 
-// Определяем кол-во столбцов N
-int N = array.GetLength(1); 
-// проверка присваемого числа 
-//Console.WriteLine($"Определение размерности массива M[mxn]. n = {N}");
+array1=SortUbiv(array1);
 
-double[] arrayS = new double[M]; // создаем массив для записи суммы строк
-for (int i = 0; i < M; i++)
-{
-    for (int j = 0; j < N; j++)
-    {
-    arrayS[i]  = arrayS[i] + array[i,j]; 
-    }
-    Console.WriteLine($"Сумма  {i}  строки   = {arrayS[i]}");
-}
-
-PrintARAY (arrayS) ;
-Console.WriteLine($"        ");
-Console.WriteLine($" Длина массива суммы строк {arrayS.Length}       ");
-
-//double Min( double[] arrayS1)
-//{
-//    for (int d = 0; d < arrayS1.Length ; d++)
-//    {
-//    double result = arrayS1[0] ;
-//    if ( arrayS1[d] < result ) result = arrayS1[d] ;
-//    return result ;
-//    }
-//}
-
-//Min(arrayS) ;
-double MIN2 = arrayS.Min() ;
-
-Console.WriteLine($"Min значение суммы строк =  {MIN2} ");
-
-double min3 = arrayS[0] ; 
-//int z = 0;
-int x = 0 ;
-for (int z = 0; z < arrayS.Length; z++)
-{
-    if (arrayS[z] < min3)
-    {
-    min3 = arrayS[z];
-    x = z;
-    }
-}
-
-Console.WriteLine($"Минимальное значение массива суммы строк {MIN2} Его индекс = {x} ");
-
-Console.WriteLine($"Ответ: Строка начального массива m x n с min суммой элементов = {MIN2} имеет индекс [{x} ] ");
+PrintARAY(array1) ;
 
 Console.WriteLine($"End ");
 
