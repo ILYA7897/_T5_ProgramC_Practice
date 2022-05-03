d05a2c4489d8b9fa353c33080e9f4750a7a742ee
«ILYA7897» <belobokov_ilya@mail.ru>
2022-05-03 09:34:22 +0300

    выполнение дз  по семинару 11 задача 11_1

---
+++ T5_sem_11/Sem_11_HW_1/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Семинар 11. от простого к практике. Домашние задание №11
+Задача 1: Определите, является ли число степенью двойки:
+N = 16 -> "Является степень двойки"
+N = 12 -> “Не является степенью двойки” */
+
+void NumberisPowerOf2(int x)
+{
+    if ((x != 0) && ((x & (x - 1)) == 0))
+    {
+    Console.WriteLine($"N - Является степенью двойки") ;
+    }
+    else Console.WriteLine($"N - Не является степенью двойки") ;
+}
+Console.Write("Введите N = ");
+var x = int.Parse(Console.ReadLine());
+NumberisPowerOf2(x) ;
+Console.WriteLine($"End") ;
+
+ 
